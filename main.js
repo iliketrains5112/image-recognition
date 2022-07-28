eb664c654ffc6683afb45d18766b260ebfcc0473
iliketrains <84407492+iliketrains5112@users.noreply.github.com>
2022-07-28 16:35:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,3 +22,24 @@ Webcam.snap(
 console.log("Ml5 version: "+ml5.version);
 
 classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Z498Dlc6t/model.json', modelLoaded);
+
+function modelLoaded(){
+    console.log("Model is loaded")
+}
+
+function check(){
+img=document.getElementById("result_image")
+classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+if (error){
+    console.error(error)
+}
+else{
+    console.log(results)
+    document.getElementById("result_object_name").innerHTML=results[0].label
+    document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3)
+}
+
+}
